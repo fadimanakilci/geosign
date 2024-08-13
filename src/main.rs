f2fe1d86fab376a190c0a7f47bd6810d1b4b4f2a
fadimanakilci <fadimekilci07@gmail.com>
2024-08-13 10:19:58 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright © August 2024 Fadimana Kilci - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Created by Fadimana Kilci  <fadimekilci07@gmail.com>, August 2024
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
