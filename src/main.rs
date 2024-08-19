2908ddefce92902d3c53d11452b3b0080f0539ea
fadimanakilci <fadimekilci07@gmail.com>
2024-08-19 09:15:19 +0300

    refactor: refactor the add vectors function to fit the data from the database

--- src/main.rs
+++ src/main.rs
@@ -149,18 +149,104 @@ async fn create_collection() -> Result<(), QdrantError> {
 }
 
 async fn add_vectors() -> Result<(), QdrantError> {
-    let points = vec![
-        PointStruct::new(1, vec![0.05, 0.61, 0.76, 0.74], [("city", "Berlin".into())]),
-        PointStruct::new(2, vec![0.19, 0.81, 0.75, 0.11], [("city", "London".into())]),
-        PointStruct::new(3, vec![0.36, 0.55, 0.47, 0.94], [("city", "Moscow".into())]),
-        // ..truncated
-    ];
+    // let points = vec![
+    //     PointStruct::new(1, vec![0.05, 0.61, 0.76, 0.74], [("city", "Berlin".into())]),
+    //     PointStruct::new(2, vec![0.19, 0.81, 0.75, 0.11], [("city", "London".into())]),
+    //     PointStruct::new(3, vec![0.36, 0.55, 0.47, 0.94], [("city", "Moscow".into())]),
+    //     // ..truncated
+    // ];
+
+    let global_rows = data_rows.lock().unwrap();
+    let mut points: Vec<PointStruct> = Vec::new();
+    let mut payload_map: HashMap<String, Value> = HashMap::new();
+
+    if let Some(ref rows) = *global_rows {
+        for row in rows.iter() {
+            let mut latitude: f32 = 0.0;
+            let mut longitude: f32 = 0.0;
+
+            match parse_coordinate(row.get(11)) {
+                Ok((lat, long)) => {
+                    latitude = lat;
+                    longitude = long;
+                },
+                Err(e) => {
+                    eprintln!("Error parsing coordinate: {}", e);
+                }
+            }
+
+            // // JSON formatında veriyi hazırla
+            // let point = QdrantPoint {
+            //     vector: [latitude, longitude],
+            //     payload: Payload {
+            //         id: row.get(0),
+            //         index: row.get(1),
+            //         device_id: row.get(2),
+            //         vehicle_id: row.get(3),
+            //         user_id: row.get(4),
+            //         m_code: row.get(5),
+            //         mt_id: row.get(6),
+            //         con_type: row.get(7),
+            //         device_time: DateTime::parse_from_rfc3339(row.get(8)).unwrap(),
+            //         server_time: DateTime::parse_from_rfc3339(row.get(9)).unwrap(),
+            //         locale: row.get(10),
+            //         coordinate: row.get(11),
+            //         ignition_on: row.get(12),
+            //         speed: row.get(13),
+            //         distance: row.get(14),
+            //         total_distance: row.get(15),
+            //         engine_hours: row.get(16),
+            //     },
+            // };
+
+            let speed_text: String = row.get(13);
+            let distance: String = row.get(14);
+            let total_distance: String = row.get(15);
+            let numeric_value_speed = f64::from_str(&speed_text).unwrap_or_default();
+            let numeric_value_distance = f64::from_str(&distance).unwrap_or_default();
+            let numeric_value_total = f64::from_str(&total_distance).unwrap_or_default();
+
+            payload_map.insert("id".to_string(), Value::from(row.get::<_, i32>(0) as i64));
+            payload_map.insert("index".to_string(), Value::from(row.get::<_, i32>(1) as i64));
+            payload_map.insert("device_id".to_string(), Value::from(row.get::<_, String>(2)));
+            payload_map.insert("vehicle_id".to_string(), Value::from(row.get::<_, String>(3)));
+            payload_map.insert("user_id".to_string(), Value::from(row.get::<_, String>(4)));
+            payload_map.insert("m_code".to_string(), Value::from(row.get::<_, String>(5)));
+            payload_map.insert("mt_id".to_string(), Value::from(row.get::<_, String>(6)));
+            payload_map.insert("con_type".to_string(), Value::from(row.get::<_, String>(7)));
+            payload_map.insert("device_time".to_string(), Value::from(row.get::<_, String>(8))); // Tarihi string olarak ekliyoruz
+            payload_map.insert("server_time".to_string(), Value::from(row.get::<_, String>(9))); // Tarihi string olarak ekliyoruz
+            payload_map.insert("locale".to_string(), Value::from(row.get::<_, String>(10)));
+            payload_map.insert("coordinate".to_string(), Value::from(row.get::<_, String>(11)));
+            payload_map.insert("ignition_on".to_string(), Value::from(row.get::<_, bool>(12)));
+            payload_map.insert("speed".to_string(), Value::from(numeric_value_speed));
+            payload_map.insert("distance".to_string(), Value::from(numeric_value_distance));
+            payload_map.insert("total_distance".to_string(), Value::from(numeric_value_total));
+            payload_map.insert("engine_hours".to_string(), Value::from(row.get::<_, i32>(16) as i64));
+
+            let id: String = row.get::<usize, i64>(0).to_string();
+            let point_id: PointId = PointId::from(id);
+
+            let point = PointStruct::new(
+                point_id,
+                // vec![latitude, longitude],
+                vec![latitude, longitude],
+                payload_map.clone());
+
+            points.push(point);
+        }
+    }
 
     let response = client
-        .upsert_points(UpsertPointsBuilder::new("test_collection", points).wait(true))
+        .upsert_points(UpsertPointsBuilder::new("geo_collection", points).wait(true))
         .await?;
 
-    dbg!(response);
+    // // Veriyi Qdrant'a gönder
+    // let response = http_client
+    //     .post(qdrant_url)
+    //     .json(&point_data)
+    //     .send()
+    //     .await;
 
     Ok(())
 }
